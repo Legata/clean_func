3fdbc43af38b44fc0d739d9e18244732b6c6155e
Legata <legata93@gmail.com>
2023-10-10 22:51:36 +0300

    appveyor1.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,23 @@ module.exports = {
     jest: true,
   },
   extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
+  },
+  rules: {
   },
 };
