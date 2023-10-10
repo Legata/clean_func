d9593dda5fe156666ca1ede7539fae9444f9b899
Legata <legata93@gmail.com>
2023-10-10 22:08:13 +0300

    fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true
-    },
-    "extends": "airbnb-base",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,11 @@
 
 // console.log(sum([1, 2]));
 
-
 export default function setHealth(hp) {
-    if (hp.health > 50) {
-        return "healthy";
-    } else if (hp.health <= 50 && hp.health > 15) {
-        return "wounded";
-    } else {
-        return "critical";
-    }
+  if (hp.health > 50) {
+    return 'healthy';
+  } if (hp.health <= 50 && hp.health > 15) {
+    return 'wounded';
+  }
+  return 'critical';
 }
